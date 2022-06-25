37c7e82c922ab2b8bce53efe3173e587bdda16b8
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-06-25 05:55:28 +0530

    Time: 76 ms (98.22%), Space: 20.8 MB (78.41%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,15 +1,7 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        def no_of_set_bits(n):
-            cnt_1s = 0
-            for i in range(32):
-                mask = 1 << i
-                if mask & n:
-                    cnt_1s += 1
-            return cnt_1s
-        
-        res = []
-        for i in range(n+1):
-            val = no_of_set_bits(i)
-            res.append(val)
-        return res
+        check = [0]
+        for i in range(1,n+1):
+            val = i % 2
+            check.append(check[i//2] + val)
+        return check
