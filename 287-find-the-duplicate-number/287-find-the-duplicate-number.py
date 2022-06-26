a87c13beb0d3344f71582235032f07efaeabfb52
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-06-26 08:20:17 +0530

    Time: 1140 ms (23.18%), Space: 35.8 MB (6.66%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -7,8 +7,20 @@ class Solution:
         #         return i
         #     prev = i
             
-        seen = [0] * (len(nums)+1)
-        for num in nums:
-            if seen[num]:
-                return(num)
-            seen[num] = 1
+        # seen = [0] * (len(nums)+1)
+        # for num in nums:
+        #     if seen[num]:
+        #         return(num)
+        #     seen[num] = 1
+            
+        hashmap = {}
+        for i in nums:
+            if i not in hashmap:
+                hashmap[i] = 1
+            else:
+                hashmap[i] += 1
+                
+                
+        for i in hashmap:
+            if hashmap[i] > 1:
+                return i
