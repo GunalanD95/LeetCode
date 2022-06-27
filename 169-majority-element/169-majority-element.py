4bb5879b239936d3aaabafe5edff5535077a769c
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-06-27 08:07:34 +0530

    Time: 324 ms (14.43%), Space: 15.5 MB (34.50%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -4,12 +4,21 @@ class Solution:
         c = 0
         
         for i in nums:
+            if c == 0:
+                me = i
+                
             if i == me:
                 c += 1
-            else:
-                if c == 0:
-                    me = i
-                else:
-                    c -= 1
-        return me
+                
+            elif i != me:
+                c -= 1
+                
+
+        
+        cc = 0
+        for j in nums:
+            if j == me:
+                cc += 1   
+        if cc > len(nums)/2:
+            return me
         
