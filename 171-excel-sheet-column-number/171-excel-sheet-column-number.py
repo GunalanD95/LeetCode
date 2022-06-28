a246c5d8713560c7599fd9b250bcc18b9facc686
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-06-28 18:44:08 +0530

    Time: 72 ms (6.96%), Space: 13.9 MB (55.90%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        s = len(columnTitle) - 1
+        
+        powal = 0
+        ans = 0
+        while s >= 0:
+            val = ord(columnTitle[s]) - 64
+            ans += val * pow(26,powal)
+            powal += 1
+            s -= 1
+        return ans
+        
