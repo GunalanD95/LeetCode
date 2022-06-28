2318dc291bbe3a1e68f72cbd807a4cbfd3d0d610
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-06-28 06:30:47 +0530

    Time: 54 ms (31.30%), Space: 13.9 MB (7.72%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,15 +1,10 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        n = int(n)
-        # def checkbit(i,n):
-        #     mask = 1 << i
-        #     return mask & n
-        
+        n = int(n)        
         c = 0
         for i in range(32):
-            # res = checkbit(i,n)
             
-            if (1 << i) & n > 0:
+            if (1 << i) & n:
                 c += 1
                 
         return c
