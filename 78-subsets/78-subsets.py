6aae9715197cb1745a6a60a04ecd7d0dc47ea36b
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-07-16 09:23:46 +0530

    Time: 48 ms (62.17%), Space: 14.1 MB (33.99%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        N = len(nums)
+        rng = 1 << N
+
+        def checkbit(n,j):
+            if (1 << j) & n:
+                return True
+            return False
+
+
+        res = []
+        for i in range(rng):
+            cur = []
+            for j in range(N):
+                if checkbit(i,j):
+                    cur.append(nums[j])
+            res.append(cur)
+
+        return res        
