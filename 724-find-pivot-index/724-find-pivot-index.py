967a6fc18489840e6e52257ab163987fbd40fc39
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-07-17 21:17:14 +0530

    Time: 169 ms (84.98%), Space: 15.2 MB (48.62%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -1,28 +1,32 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        A = nums
-        N = len(A)
+        N = len(nums)
+        
         def cPF(A):
-            PF = list(range(0,N))
+            N = len(A)
+            PF = [0] * len(A)
             PF[0] = A[0]
+            prev = A[0]
             for i in range(1,N):
-                PF[i] = PF[i-1] + A[i]
+                PF[i] = A[i] + prev
+                prev = PF[i]
+                
             return PF
-
-        PF = cPF(A)
-
-
-        for i in range(0,N):
-
-            LFS = 0 
-            RFS = 0
+        
+        PF = cPF(nums)
+        
+        for i in range(N):
             if i == 0:
-                LFS = 0
-                RFS = PF[N-1] - PF[i]
+                leftsum = 0
+                rightsum = PF[N-1] - PF[i]
+            elif i == N:
+                leftsum = PF[i-1]
+                rightsum = 0
             else:
-                LFS = PF[i-1]
-                RFS = PF[N-1] - PF[i]
-
-            if LFS == RFS:
+                leftsum = PF[i-1]
+                rightsum = PF[N-1] - PF[i]
+                
+            if leftsum == rightsum:
                 return i
+            
         return -1
