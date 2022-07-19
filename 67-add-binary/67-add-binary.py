2e1297bfbdb8dcf16a4a936702a8561c0746f6cc
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-07-19 18:44:06 +0530

    Time: 68 ms (17.27%), Space: 13.9 MB (72.46%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,38 +1,32 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        A , B = a,b
-        A = list(A)
-        B = list(B)
-
-        A = [int(i) for i in A]
-        B = [int(j) for j in B]
-
-        N = len(A)
-        M = len(B)
-
-        if N !=  M:
-            if N > M:
-                while M < N:
-                    B.insert(0,0)
-                    N = len(A)
-                    M = len(B)
-            else:
-                while N < M:
-                    A.insert(0,0)
-                    N = len(A)
-                    M = len(B)
-        s = len(A) - 1
-        res = list(range(N))
-        car = 0
-        while s >= 0:
-            # use mod and div to get the carry and calculate the sum
-            res[s] = (A[s] + B[s] + car) % 2
-            car = (A[s] + B[s] + car) // 2
-
-            s -=  1
-
-        if car == 1:
-            res.insert(0,1)
-
-        return "".join(str(i) for i in res)
+        
+        res = ''
+        
+        a = a[::-1]
+        b = b[::-1]
+        
+        M = len(a)
+        N = len(b)
+        
+        
+        tsum = 0
+        carry = 0
+        for i in range(max(M,N)):
+            valA = ord(a[i]) - ord('0') if i < M else 0
+            valB = ord(b[i]) - ord('0') if i < N else 0
+            
+            tsum = valA + valB + carry
+            char = str(tsum % 2)
+            res = char + res
+            carry = tsum // 2
+            
+            
+        if carry:
+            res = '1' + res
+            
+        return res
+            
+                
+            
         
