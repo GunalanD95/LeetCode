9b814ce3ca3311dfde0519bebe44f81001ba892f
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-07-21 10:15:32 +0530

    Time: 52 ms (50.52%), Space: 13.9 MB (15.16%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -6,25 +6,29 @@ class Solution:
         l = 0
         m = 0
         h = len(nums) - 1
-        
-        while m <=h:
+
+        while m <= h:
             if nums[m] == 1:
                 m += 1
                 
+                
             elif nums[m] == 0:
-                temp    = nums[m]
+                temp = nums[m]
                 nums[m] = nums[l]
                 nums[l] = temp
                 m += 1
                 l += 1
                 
+                
+                
             elif nums[m] == 2:
-                temp    = nums[m]
+                temp = nums[m]
                 nums[m] = nums[h]
                 nums[h] = temp
                 h -= 1
                 
-        return nums
                 
+        return nums
+            
                 
         
