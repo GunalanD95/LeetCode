64daac2ee2310af282d1746eefcb0798a1ebde9a
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-08-09 20:17:21 +0530

    Time: 46 ms (73.28%), Space: 14 MB (31.86%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,29 +1,14 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         
-        smap =  {}
-        tmap =  {}
-
-        for i in range(len(s)):
-            if s[i] not in smap:
-                smap[s[i]] = 1
-            else:
-                smap[s[i]] += 1
-
-
-        for i in range(len(t)):
-            if t[i] not in tmap:
-                tmap[t[i]] = 1
-            else:
-                tmap[t[i]] += 1
-            
-            
-            
-        for k in tmap:
-            if k not in smap:
-                return k
-            
-            else:
-                if tmap[k] != smap[k]:
-                    return k
+        ans = 0
         
+        for i in range(len(s)):
+            ans ^= ord(s[i])
+            
+            
+        for j in range(len(t)):
+            ans ^= ord(t[j])
+            
+            
+        return(chr(ans))
