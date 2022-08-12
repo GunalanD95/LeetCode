342d0288e2bae81d85302ef288aa37acf49c6126
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-08-12 12:39:49 +0530

    Time: 42 ms (68.86%), Space: 13.9 MB (68.27%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,17 +1,36 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def calpower(x,n):
-            if n == 0:
-                return 1
+#         def calpower(x,n):
+#             if n == 0:
+#                 return 1
             
-            if n < 0:
-                return 1 / calpower(x, -n)
+#             if n < 0:
+#                 return 1 / calpower(x, -n)
             
-            hp = calpower(x,n//2)
+#             hp = calpower(x,n//2)
             
-            if n % 2 == 0:
-                return hp * hp
-            else:
-                return hp * hp * x
+#             if n % 2 == 0:
+#                 return hp * hp
+#             else:
+#                 return hp * hp * x
+
+        # iterative approach 
+        def calpower(a,n):
+            res =  1
             
-        return calpower(x,n)
+            while n > 0:
+                if n % 2 == 1:
+                    res = res * a
+                a = a * a
+                n = n // 2
+                
+            return res
+            
+        
+        
+        if n <  0:
+            val = 1 /calpower(x,- n)
+        else:
+            val = calpower(x,n)
+        
+        return val
