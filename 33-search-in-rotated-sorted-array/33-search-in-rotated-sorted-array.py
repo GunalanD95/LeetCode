0442d621b464b00bc71b58d6a154ffd108c72076
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-09-09 23:11:14 +0530

    Time: 83 ms (17.43%), Space: 14.3 MB (18.05%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,57 +1,32 @@
 class Solution:
-    def binarysearch(self,arr,l,r,k):
-        low , high = l , r
+    def search(self, nums: List[int], target: int) -> int:
         
-        while low <= high:
+        low = 0 
+        
+        high = len(nums) - 1
+        
+        
+        while low <= high :
+            
             mid = (low + high)//2
+            if nums[mid] == target: return mid
             
-            if arr[mid] == k: return mid
-            
-            if arr[mid] > k: high = mid - 1
-            else: low =  mid + 1
+            if nums[low] <= nums[mid]:
+                
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+                    
+                    
+            else:
+
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                    
+                else:
+                    high = mid - 1
+                
                 
         return -1
-            
-        
-        
-    
-    def findPOR(self,arr):
-        low , high = 0 , len(arr) - 1
-
-        low = 0
-        high = len(arr) - 1
-        while low <= high:
-
-            mid = low + (high - low)//2
-
-            if arr[mid] < arr[high]:
-                high = mid
-
-            else:
-                low = mid + 1
-
-        return mid
-            
-    
-    
-    def search(self, nums: List[int], target: int) -> int:
-        N = len(nums) 
-        
-        if N == 1:
-            if nums[0] == target:
-                return 0
-            else:
-                return -1
-        
-        POR = self.findPOR(nums)
-        
-        index1 = self.binarysearch(nums,0,POR-1,target)
-        if index1 != -1:
-            return index1
-        
-        index2 = self.binarysearch(nums,POR,N-1,target)
-        if index2 != -1:
-            return index2            
-        return -1
-            
         
