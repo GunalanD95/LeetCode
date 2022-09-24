cd32f34f53dc29a6e6c493b68d8c9de3beb1be02
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-09-24 17:33:24 +0530

    Time: 84 ms (36.05%), Space: 19.4 MB (10.74%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        stack = []
+        cur_stack = []
+        
+        def dfs(node,cur_sum):
+            if not node:
+                return
+        
+            cur_sum += node.val
+            cur_stack.append(node.val)
+            
+            temp = cur_stack.copy()
+            
+            
+            if not node.left and not node.right:
+                if cur_sum == targetSum:
+                    stack.append(temp) 
+                    
+            left = dfs(node.left,cur_sum)
+            right = dfs(node.right,cur_sum)
+            
+            cur_stack.pop()
+            
+                
+                
+        dfs(root,0)
+        return stack
+        
