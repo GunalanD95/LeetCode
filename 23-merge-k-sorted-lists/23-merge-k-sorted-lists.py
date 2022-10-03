96c5c485e0a20e8a080a802723b1615425648c88
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-10-03 19:57:14 +0530

    Time: 233 ms (34.49%), Space: 17.6 MB (93.52%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -1,7 +1,9 @@
 class Solution:
     def mergeLL(self,list1,list2):
         dummy = ListNode(-1)
-        cur = dummy 
+        
+        cur = dummy
+        
         while list1 and list2:
             if list1.val < list2.val:
                 cur.next = list1
@@ -16,19 +18,23 @@ class Solution:
             cur.next = list1
         else:
             cur.next = list2
+            
+            
         return dummy.next
         
     
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        N = len(lists)
-        if N == 0: return None
-        if N == 1: return lists[0]
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        l =  self.mergeKLists(lists[:mid]) 
         
-        root = lists[0]
-        for i in range(1,N):
-            cur   = lists[i]
-            root  = self.mergeLL(root,cur)
-        return root 
+        r =   self.mergeKLists(lists[mid:])
+        
+        
+        return self.mergeLL(l, r)
             
             
             
