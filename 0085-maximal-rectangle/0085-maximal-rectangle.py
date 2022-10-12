5d15118d741d700b6479c1f866e2a22b8bcc8639
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-10-12 21:25:10 +0530

    Time: 551 ms (55.67%), Space: 15.3 MB (55.72%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -0,0 +1,50 @@
+class Solution:
+    def largestRectangleArea(self,heights):
+        N = len(heights)
+        if N == 1: return heights[0]
+
+        left = [-1] * N
+        lstack = []
+
+        for i in range(N):
+            while lstack and heights[i] <= heights[lstack[-1]]:
+                lstack.pop()
+            if lstack:
+                left[i] = lstack[-1]
+            lstack.append(i)
+
+        right = [N] * N
+        rstack = []
+        for i in range(N-1,-1,-1):
+            while rstack and heights[i] <= heights[rstack[-1]]:
+                rstack.pop()
+            if rstack:
+                right[i] = rstack[-1]
+            rstack.append(i)
+
+        ans = float('-inf')
+
+
+        for i in range(N):
+            val = heights[i] * (right[i] - left[i] - 1)
+
+            ans = max(ans,val)
+
+        return ans
+
+    
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        ans = [0] * cols
+
+        res = float('-inf')
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1': ans[j] += 1
+                else: ans[j] = 0
+            value = self.largestRectangleArea(ans)
+            res = max(res,value)
+            
+            
+        return res
