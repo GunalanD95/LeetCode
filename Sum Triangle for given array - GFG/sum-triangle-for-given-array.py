db03c8d5807132efdbabeadc9a2ab6e26d2fbc0a
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-10-26 08:22:59 +0530

    Added solution - LeetHub

---
+++ Sum Triangle for given array - GFG/sum-triangle-for-given-array.py
@@ -0,0 +1,54 @@
+#User function Template for python3
+
+def getTriangle(arr, n):
+    ans = []
+    def sumoftriangle(arr):
+        if len(arr) == 1:
+            # ans.append(arr[0])
+            return 
+    
+        newarr = []
+        prev = arr[0]
+        for i in range(1,len(arr)):
+            val  = prev + arr[i]
+            newarr.append(val)
+            prev = arr[i]
+         
+        sumoftriangle(newarr) 
+        if(newarr):
+            ans.extend(newarr)
+            
+    sumoftriangle(arr)
+    ans.extend(arr)
+    return ans
+    
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        Triangle = getTriangle(a, n)
+        print(*Triangle)
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
