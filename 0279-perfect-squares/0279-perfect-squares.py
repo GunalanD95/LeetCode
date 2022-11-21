0c94e00b27a1b41d12edea4970f191d689b7f93b
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-11-21 08:02:42 +0530

    Time: 6652 ms (11.42%), Space: 23 MB (13.91%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+class Solution:    
+    def dpSquare(self,num,dp):
+        if dp[num] != -1:
+            return dp[num]
+        i = 1
+        count = num
+        while i * i <= num:
+            dpCount = self.dpSquare(num - (i*i),dp)
+            count   = min(count,dpCount + 1)
+            i += 1
+        dp[num]   = count
+        return count 
+    def numSquares(self, n: int) -> int:
+        dp = [-1] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        return self.dpSquare(n,dp)
+        
+        
+                
+            
+            
