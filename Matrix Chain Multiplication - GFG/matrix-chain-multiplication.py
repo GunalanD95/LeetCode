8068f11b35f7fccc0a07feb7f565b76db05e446f
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-12-08 19:47:45 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
@@ -1,32 +1,33 @@
 #User function Template for python3
-from functools import lru_cache
+
 class Solution:
     def matrixMultiplication(self, N, arr):
         # code here
-        N = len(arr)
+        dp = [[-1]*(N+1) for i in range(N+1)]
         
-        @lru_cache(None)
-        def getMeMatrix(i,j):
+        
+        def getmeMatrix(i,j):
             if i == j:
                 return 0
                 
-
+                
+            if dp[i][j] != -1:
+                return dp[i][j]
+                
+                
             min_val = float('inf')
             
-            # loop will run from i --> (j-1)
+            # loop from i to j -1
             for k in range(i,j):
-                
-                value = (arr[i-1] * arr[k] * arr[j]) + ( getMeMatrix(i,k) + getMeMatrix(k+1,j) )
-                
+                value = (arr[i-1] * arr[k] * arr[j]) + getmeMatrix(i,k) + getmeMatrix(k+1,j)
                 min_val = min(min_val,value)
- 
                 
-            return min_val
+            dp[i][j] =   min_val  
             
-        # print("getMeMatrix",getMeMatrix(1,N-1))
-        return getMeMatrix(1,N-1)
-                
+            return dp[i][j]
 
+
+        return getmeMatrix(1,N-1)
 
 #{ 
  # Driver Code Starts
