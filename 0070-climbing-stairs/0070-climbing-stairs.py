99f59cc72710f8b9ebe778d074491640c71e7ffc
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-12-12 06:42:31 +0530

    Time: 24 ms (99.00%), Space: 14 MB (12.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,26 @@
+
 class Solution:
-    def climbStairs(self, A: int) -> int:
-        # bottom up approach
-        A = A+1
-        dp = [None] * (A+1)
-        dp[0] , dp[1] = 0 , 1
-
-        for i in range(2,A+1):
-            dp[i] = dp[i-1] + dp[i-2]
-
-        return dp[A]        
+    def climbStairs(self, n: int) -> int:
+        
+        dp = [-1] * (n+1)
+        
+        def fibo(n,dp_arr):
+            # base case
+            if n <= 1:
+                return 1
+            
+            # result not yet calculated
+            if dp_arr[n] == -1:
+                dp_arr[n] = fibo(n-1,dp) +  fibo(n-2,dp)
+                
+            return dp_arr[n]
+        
+        
+        return fibo(n,dp)
+            
+        
+        
+        "fib(n) = fib(n-1) + fib(n-2)"
+        'ans    = prev_ans + prev_p_ans'
+        
+        
