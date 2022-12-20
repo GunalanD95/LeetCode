47494256add3c568fbfbc5d63218f9b508d8f412
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-12-20 06:50:34 +0530

    Time: 67 ms (93.24%), Space: 14.9 MB (28.52%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dfs(self,node,visited,adj):
+        visited[node] = True 
+
+        for child in adj[node]:
+            if not visited[child]:
+                self.dfs(child,visited,adj)
+
+
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        N = len(rooms)
+        visited = [False] * (N)
+
+        self.dfs(0,visited,rooms)
+
+        if False in visited:
+            return False
+        return True        
