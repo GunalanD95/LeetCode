c07e658a5520a7371cca9b31bc6b7f9400bfa6d7
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2022-12-25 08:31:47 +0530

    Time: 131 ms (85.44%), Space: 14.2 MB (40.29%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,17 +1,35 @@
 class Solution:
-    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+    def searchInsert(self,nums,target):
+        low   = 0 
+        high  = len(nums) - 1
         
-        ans = []
-        for i in queries:
-            val = i
-            cur_sum = 0
-            c = 0
-            nums.sort()
-            for j in range(len(nums)):
-                if cur_sum + nums[j] <= val:
-                    c +=1
-                cur_sum += nums[j] 
-
-            ans.append(c)
+        while low <= high:
+            mid = (low + high) // 2
             
-        return ans 
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] > target:
+                high = mid -1 
+            else:
+                low  = mid + 1
+                
+        return high
+        
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        n  = len(nums)
+        
+        nums.sort()
+        pf = [0] * (n)
+        pf[0] = nums[0]
+        
+        for i in range(1,n):
+            pf[i] = pf[i-1] + nums[i]
+            
+        ans = []
+        
+        for q in queries:
+            indx = self.searchInsert(pf,q)
+            ans.append(indx+1)
+            
+        return ans         
