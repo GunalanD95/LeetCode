dd6df754dc6fea527739872147c860e88c1062d1
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-01-18 20:24:26 +0530

    Time: 784 ms (93.30%), Space: 20.3 MB (81.23%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -3,26 +3,20 @@ from collections import defaultdict
 import heapq as hq
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        minHeap = []
+        N = len(points)
         
-        min_val = float('inf')
-        hashmap = defaultdict(list)
-        for point in points:
-            x1 , y1  = abs(point[0]) , abs(point[1])
-            
-            sqrt_sum = (x1*x1) + (y1*y1)
-            hashmap[sqrt_sum].append(point)
-            
-            hq.heappush(minHeap,sqrt_sum)
+        min_heap = []
+        
+        for i in range(N):
+            sqrt = ( points[i][0] * points[i][0] ) + (points[i][1] * points[i][1])
+            hq.heappush(min_heap, (sqrt,points[i]))
             
             
         ans = []
-        
-        while k != 0:
-            key = hq.heappop(minHeap)
-            ans.extend(hashmap[key])
-            del hashmap[key]
-            k -=1
+        while k > 0:
+            val , point = hq.heappop(min_heap)
+            ans.append(point)
+            k -= 1
             
-        return ans 
+        return ans
         
