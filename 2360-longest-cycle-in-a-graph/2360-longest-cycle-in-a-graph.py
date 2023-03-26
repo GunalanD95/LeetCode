4780c183b4b54c6d914fbf76bcd6525699563de9
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-03-26 10:56:03 +0530

    Time: 1900 ms (18.38%), Space: 150.5 MB (43.93%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -0,0 +1,34 @@
+class Solution:
+    def dfs(self,node,visited,adj,distance,steps):
+        visited[node] = True
+        for child in adj[node]:
+            if not visited[child]:
+                distance[child] = steps+1
+                self.dfs(child,visited,adj,distance,steps+1)
+                distance[child] = -(distance[child])
+            else:
+                if distance[child] > 0:
+                    self.max_len = max(self.max_len,steps+1 - distance[child])
+                    distance[child] = -(distance[child])
+                else:
+                    return 
+
+    def longestCycle(self, edges: List[int]) -> int:
+        n = len(edges)
+        self.max_len = -1
+        visited  = [False] * (n)
+        adj = [[] for _ in range(n)]
+        distance = [0] * (n)
+        
+        for idx,val in enumerate(edges):
+            if val !=-1:
+                adj[idx].append(val)
+                
+        for node in range(n):
+            if not visited[node]:
+                distance[node] = 1
+                self.dfs(node,visited,adj,distance,1)
+                distance[node] = -1
+        return self.max_len
+            
+                
