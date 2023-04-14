bca2e23f44aba564d9ee085d1954773a9e69dbe4
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-04-14 07:09:26 +0530

    Time: 1686 ms (49.08%), Space: 62.2 MB (27.58%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,27 +1,23 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        text1 = s
-        text2 = s[::-1]
-        n , m = len(text1) , len(text2)
-        
-        dp = [[-1] * (m+1) for i in range(n+1)]
-        
-        def DPRecursion(indx1,indx2):
-            if indx1 < 0 or indx2 < 0:
+        N = len(s)  
+        dp = [[-1] * (N+1) for _ in range(N+1)]
+        def DP(indx1,indx2):
+            if indx1 >= N or indx2 < 0:
                 return 0
             
             if dp[indx1][indx2] != -1:
                 return dp[indx1][indx2]
             
-            if text1[indx1] == text2[indx2]:
-                dp[indx1][indx2] =  1 + DPRecursion(indx1-1,indx2-1)
             
+            if s[indx1] == s[indx2]:
+                dp[indx1][indx2] = DP(indx1+1,indx2-1) + 1
+                
             else:
-                dp[indx1][indx2] = max(DPRecursion(indx1-1,indx2) , DPRecursion(indx1,indx2-1))
-                
-                
+                dp[indx1][indx2] = max (
+                                       DP(indx1+1,indx2) ,
+                                       DP(indx1,indx2-1) 
+                )
             return dp[indx1][indx2]
-            
-            
-            
-        return DPRecursion(n-1,m-1)        
+
+        return DP(0,N-1)
