01294b4903702985a88033255b54558254576f3c
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-05-15 07:29:55 +0530

    Time: 943 ms (93.30%), Space: 51 MB (15.61%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -1,21 +1,21 @@
 class Solution:
-    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    def swapNodes(self, head, k) :
         front_node = self.findKthNode(head, k)
         end_node   = self.findKthNodeFromEnd(head, k)
         front_node.val, end_node.val = end_node.val, front_node.val
         return head
 
-    def findKthNode(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    def findKthNode(self, head, k) :
         node = head
         for _ in range(k-1):
             node = node.next
         return node
     
-    def findKthNodeFromEnd(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        slow_node = fast_node = head
+    def findKthNodeFromEnd(self, head, k) :
+        slow = fast = head
         for _ in range(k-1):
-            fast_node = fast_node.next
-        while fast_node.next:
-            slow_node = slow_node.next
-            fast_node = fast_node.next
-        return slow_node
+            fast = fast.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+        return slow
