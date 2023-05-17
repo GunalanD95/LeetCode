c1bc90291c51caceac9b525d2236b563363388e7
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-05-17 08:10:25 +0530

    Time: 849 ms (79.85%), Space: 47.6 MB (53.83%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -1,16 +1,45 @@
 class Solution:
-    def pairSum(self, head: Optional[ListNode]) -> int:
-        indexMap = {}
-        N = 0
-        temp = head
+    def findMid(self,head):
+        prev = None
+        slow = fast = head 
         
-        while temp:
-            indexMap[N] = temp.val
-            temp = temp.next
-            N += 1
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
             
+        return prev , slow 
+    
+    
+    def reverse(self,head):
+        prev = None
+        cur  = head
+        cn   = None
+        
+        while cur:
+            cn = cur.next
+            cur.next = prev
+            prev = cur
+            cur  = cn
+            
+        return prev
+    
+    
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        
+        h1_tail , h2 = self.findMid(head)
+        h1_tail.next = None
+        
+        h2 = self.reverse(h2)
+        
         max_sum = 0
-        for i in range(N):
-            cur_sum = (indexMap[i] + indexMap[N-i-1])
+        while head and h2:
+            cur_sum = head.val + h2.val
             max_sum = max(max_sum,cur_sum)
-        return max_sum    
+            
+            head = head.next
+            h2   = h2.next
+            
+        return max_sum
+        
+          
