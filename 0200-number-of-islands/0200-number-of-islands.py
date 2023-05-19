a78d1cb19b040a36f9de0a040f63850e15296e7b
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-05-19 07:12:10 +0530

    Time: 322 ms (54.26%), Space: 18.8 MB (54.10%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,33 @@
+from collections import deque
+
 class Solution:
-    def backtracking(self,i,j,mat,N,M):
-        if i < 0 or i >= N or j < 0 or j >= M:
-            return 
-        
-        if mat[i][j] == '0':
-            return 
-        
-        if mat[i][j] == '1':
-            mat[i][j] = '0'               # make it visited 
-            
-            # go top 
-            self.backtracking(i-1,j,mat,N,M)
-            # go down
-            self.backtracking(i+1,j,mat,N,M)
-            # go right
-            self.backtracking(i,j+1,mat,N,M)
-            # go left
-            self.backtracking(i,j-1,mat,N,M)
-        
-    
     def numIslands(self, grid: List[List[str]]) -> int:
-        N , M = len(grid) , len(grid[0])
+        if not grid:
+            return 0
         
-        islands = 0
-        for i in range(N):
-            for j in range(M):
-                if grid[i][j] == '1':
-                    self.backtracking(i,j,grid,N,M)
-                    islands += 1
-                     
-                    
-        return islands
-                    
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        count = 0
         
+        def bfs(row, col):
+            queue = deque([(row, col)])
+            visited[row][col] = True
+            
+            while queue:
+                r, c = queue.popleft()
+                
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '1' and not visited[nr][nc]:
+                        queue.append((nr, nc))
+                        visited[nr][nc] = True
+        
+        visited = [[False] * cols for _ in range(rows)]
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    count += 1
+                    bfs(i, j)
+        
+        return count
