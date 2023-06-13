463be1122e1f81a14e305b4f6cd9bab9cd868444
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-06-13 08:04:35 +0530

    Time: 824 ms (14.36%), Space: 21.2 MB (89.39%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -6,7 +6,7 @@ class Solution:
         
         arr = [[0]*rows for i in range(cols) ]
         
-        
+        # transpose the matrix
         for k in range(cols):
             x = 0
             y = k
@@ -15,6 +15,7 @@ class Solution:
                 arr[y][x] = grid[x][y]
                 x += 1
 
+        # check rows are same 
         c = 0
         for row in grid:
             for col in arr:
