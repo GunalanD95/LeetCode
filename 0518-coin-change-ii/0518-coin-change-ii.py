0ac83c0e0e02b8e126d49689d71b74bb19a74f78
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-08-11 07:16:58 +0530

    Time: 134 ms (93.53%), Space: 41.1 MB (27.87%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -8,8 +8,7 @@ class Solution:
         def knapSackCoin(indx, coins_used):
             if indx < 0 or coins_used < 0:
                 return 0
-            
-            
+
             if coins_used == 0:
                 return 1
             
@@ -31,3 +30,23 @@ class Solution:
             return dp[indx][coins_used]
 
         return knapSackCoin(N-1, amount)
+    
+    
+        #TABULATION
+        N = len(coins)
+        
+        dp = [[0 for _ in range(amount+1)] for _ in range(len(coins))]
+        
+        for i in range(len(coins)):
+            dp[i][0] = 1
+            
+            
+        for indx  in range(N):
+            for coins_used in range(amount+1):
+                if coins[indx] > coins_used:
+                    dp[indx][coins_used] = dp[indx-1][coins_used]
+                else:
+                    dp[indx][coins_used] = dp[indx - 1][coins_used] + dp[indx][coins_used - coins[indx]]
+            
+            
+        return(dp[-1][-1])
