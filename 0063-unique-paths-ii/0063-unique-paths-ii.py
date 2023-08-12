63167a99b4d2ef32302b81e9b412f249222c0478
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-08-12 08:25:47 +0530

    Time: 42 ms (96.82%), Space: 16.6 MB (14.54%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         i = len(obstacleGrid) # row
         j = len(obstacleGrid[0])
-        ans = []
         
         hashmap = defaultdict(set)
         
@@ -17,18 +16,9 @@ class Solution:
             if row == i-1 and col == j-1:     # ans
                 return 1
             
-
-            
-            # mat[row][col] = 1                 # mark it as visited
-            
             if (row,col) not in hashmap:
                 hashmap[(row,col)] = ratinMaze(row,col+1,mat,hashmap) + ratinMaze(row+1,col,mat,hashmap)
             
-            # mat[row][col] = 0                 # undo changes 
             return hashmap[(row,col)]
 
-                             
-            
-            
-            
         return ratinMaze(0,0,obstacleGrid,hashmap)  
