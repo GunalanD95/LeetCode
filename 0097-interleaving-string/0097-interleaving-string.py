a482c13d63ffd9901a9024c615af2804db7b3cf2
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2023-08-25 07:13:26 +0530

    Time: 46 ms (82.85%), Space: 16.4 MB (79.19%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,15 +1,18 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3): return False
+        
         N , M = len(s1) , len(s2)
-        @lru_cache(None)
-        def recurBro(indx1 , indx2):
-            if indx1 == N and indx2 == M:
-                return True
-            ans = False
-            if indx1 < N and s1[indx1] == s3[indx1 + indx2]:
-                ans |= recurBro(indx1 +1 , indx2 )
-            if indx2 < M and s2[indx2] == s3[indx1 + indx2]:
-                ans |= recurBro(indx1 , indx2 +1)
-            return ans
-        return recurBro(0,0)
+
+        dp = [[False]* (M+1) for _ in range(N+1)]
+        dp[N][M] = True
+
+        for indx1 in range(N,-1,-1):
+            for indx2 in range(M,-1,-1):
+                if indx1 < N and s1[indx1] == s3[indx1 + indx2]:
+                    dp[indx1][indx2] |= dp[indx1 +1][indx2]
+                if indx2 < M and s2[indx2] == s3[indx1 + indx2]:
+                    dp[indx1][indx2] |= dp[indx1][indx2 +1]
+
+
+        return dp[0][0]
