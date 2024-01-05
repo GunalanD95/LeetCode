0d90de743cda47a95a824a664ba0b58099d1da18
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-01-05 09:24:44 +0530

    Time: 1773 ms (67.79%), Space: 17.8 MB (14.10%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,7 +1,5 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if nums[-1] == -860:
-            return 96
         if not nums:
             return 0
         n = len(nums)
