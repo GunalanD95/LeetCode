c42d9d7f8004fba3b7a795f7495c262b7e803018
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-01-16 06:57:58 +0530

    Time: 271 ms (86.66%), Space: 64.2 MB (43.69%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -3,20 +3,33 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self._set = set()
+        self._mapper    = {}
+        self._values    = []
         
     def insert(self, val: int) -> bool:
-        if val not in self._set:
-            self._set.add(val)
-            return True
-        return False
-
+        if val in self._mapper:
+            return False
+        
+        self._values.append(val)
+        get_len = len(self._values) - 1
+        self._mapper[val] = get_len  
+        
+        return True
+        
     def remove(self, val: int) -> bool:
-        if val in self._set:
-            self._set.remove(val)
-            return True
-        return False
+        if val not in self._mapper:
+            return False
+        
+        index = self._mapper[val]
+        last_val = self._values[-1]
+
+        self._values[index] = last_val
+        self._mapper[last_val] = index
+
+        self._values.pop()
+        del self._mapper[val]
+        
+        return True
 
     def getRandom(self) -> int:
-        cur = list(self._set)
-        return random.choice(cur)
+        return random.choice(self._values)
