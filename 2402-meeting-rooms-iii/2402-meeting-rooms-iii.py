ab26c62d31ec80fc0e5b12286103a7a9a32df836
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-04-03 07:50:41 +0530

    Time: 1127 ms (96.10%), Space: 62.7 MB (50.47%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,24 +1,42 @@
+import heapq as hq
+
 class Solution:
-    def mostBooked(self, num_rooms, meetings):
-        available_rooms = [room for room in range(num_rooms)]
-        occupied_rooms = []
-        heapify(available_rooms)
-        booking_counts = [0] * num_rooms
-
-        sorted_meetings = sorted(meetings, key=lambda x: x[0])
-        for start_time, end_time in sorted_meetings:
-            while occupied_rooms and occupied_rooms[0][0] <= start_time:
-                end, room = heappop(occupied_rooms)
-                heappush(available_rooms, room)
-
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        
+        available_rooms = [room for room in range(n)]
+        used_rooms      = [] # (end_time ,room_number)
+        count = [0] * n
+        
+        
+        for start,end in meetings:
+            
+            # if there are used room and we are checking current start with latest end meeting
+            while used_rooms and start >= used_rooms[0][0]:
+                _ , room =  hq.heappop(used_rooms)
+                # now this room is free lets push to available_rooms
+                
+                hq.heappush(available_rooms,room)
+                
+                
+            # if rooms are free , then assign the current meetigs
             if available_rooms:
-                room = heappop(available_rooms)
-                heappush(occupied_rooms, [end_time, room])
+                room = hq.heappop(available_rooms)
+                count[room] += 1
+                hq.heappush(used_rooms,(end,room))
             else:
-                current_end, room = heappop(occupied_rooms)
-                new_end = current_end + end_time - start_time
-                heappush(occupied_rooms, [new_end, room])
-            booking_counts[room] += 1
-        max_booking_count = max(booking_counts)
-        most_booked_room = booking_counts.index(max_booking_count)
-        return most_booked_room
+                # no room is free 
+                end_time , room = hq.heappop(used_rooms)
+                duration = end - start 
+                hq.heappush(used_rooms,(end_time + duration ,room))
+                count[room] += 1
+                
+                
+                
+                
+                
+        max_val = max(count)
+        max_idx = count.index(max_val)
+        print(count)
+        return max_idx
+        
