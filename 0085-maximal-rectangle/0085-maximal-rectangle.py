eb461618cefd401d18cd78cd37bfc1d4a7ec893d
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-04-13 20:27:08 +0530

    Time: 223 ms (53.95%), Space: 17.9 MB (76.17%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -2,7 +2,7 @@ class Solution:
     def largestRectangleArea(self,heights):
         N = len(heights)
         if N == 1: return heights[0]
-
+        
         left = [-1] * N
         lstack = []
 
@@ -12,7 +12,7 @@ class Solution:
             if lstack:
                 left[i] = lstack[-1]
             lstack.append(i)
-
+              
         right = [N] * N
         rstack = []
         for i in range(N-1,-1,-1):
@@ -21,23 +21,21 @@ class Solution:
             if rstack:
                 right[i] = rstack[-1]
             rstack.append(i)
-
+                
         ans = float('-inf')
-
-
+        
         for i in range(N):
-            val = heights[i] * (right[i] - left[i] - 1)
-
-            ans = max(ans,val)
-
+            res = heights[i] * (right[i] - left[i] - 1)
+            ans = max(res,ans)
         return ans
-
+                
+        
+            
     
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         rows = len(matrix)
         cols = len(matrix[0])
         ans = [0] * cols
-
         res = float('-inf')
         for i in range(rows):
             for j in range(cols):
@@ -45,6 +43,4 @@ class Solution:
                 else: ans[j] = 0
             value = self.largestRectangleArea(ans)
             res = max(res,value)
-            
-            
         return res
