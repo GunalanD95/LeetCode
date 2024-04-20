1b026a7fc3ffe1018fe026123e604c3a2608931a
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-04-20 07:10:39 +0530

    Time: 814 ms (70.00%), Space: 35.9 MB (30.50%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -1,51 +1,28 @@
 class Solution:
-    def bfs(self, row, col):
-        q = deque()
-        q.append((row,col))
-
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        
-        end_row, end_col = row,col
-        self.visited.add((row,col))
-        while q:
-            cr, cc = q.popleft()                        
-            end_row, end_col = cr,cc
-            
-            for r, c in directions:
-                nr, nc = cr + r, cc + c
-                if (
-                    0 <= nr < self.rows
-                    and 0 <= nc < self.cols
-                    and self.grid[nr][nc] == 1
-                    and (nr, nc) not in self.visited
-                ):
-                    q.append((nr, nc))
-                    self.visited.add((nr, nc))
-                    
-                    
-        return end_row, end_col
-                    
-                    
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
-        if not land or not land[0]:
-            return 0
-        
-        self.rows, self.cols = len(land), len(land[0])
-        self.visited = set()
-        self.grid = land
-
+        rows, cols = len(land), len(land[0])
+        visited = set()
         total_islands = []
 
-        for row in range(self.rows):
-            for col in range(self.cols):
-                if land[row][col] == 1 and (row, col) not in self.visited:
-                    cur = [row,col]
-                    end_row,end_col = self.bfs(row, col)
-                                        
-                    cur.append(end_row)
-                    cur.append(end_col)
-                    
-                    total_islands.append(cur)
-                    
+        for row in range(rows):
+            for col in range(cols):
+                if land[row][col] == 0 or (row, col) in visited:
+                    continue
+
+                start_row, start_col = row, col
+                end_row, end_col = row, col
+
+                # Traverse rightwards to find the end column of the farmland
+                while end_col < cols and land[row][end_col] == 1:
+                    visited.add((row, end_col))
+                    end_col += 1
+
+                # Traverse downwards to find the end row of the farmland
+                while end_row < rows and land[end_row][col] == 1:
+                    for c in range(start_col, end_col):
+                        visited.add((end_row, c))
+                    end_row += 1
+
+                total_islands.append([start_row, start_col, end_row - 1, end_col - 1])
+
         return total_islands
-        
