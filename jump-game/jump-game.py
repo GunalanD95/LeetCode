4f1b77666af04e5ade246f9d170d647f1776c26d
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-04-24 08:27:43 +0530

    Time: 619 ms, Memory: 18 MB - LeetHub

---
+++ jump-game/jump-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return True
+        jumps = 1
+        n = len(nums) - 2
+        for i in range(n, -1, -1):
+            
+            if nums[i] >= jumps:
+                jumps = 1
+            else:
+                jumps += 1
+            
+            print(nums[i],jumps,"innfx",i)
+        return nums[0] >= jumps        
