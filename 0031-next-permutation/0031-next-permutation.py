f66d72d5d198afb6abf148d33d566f8d8dbd673c
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-04-24 05:59:28 +0530

    Time: 48 ms (22.18%), Space: 16.5 MB (52.98%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,26 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        indx = -1
-        
         N = len(nums)
+        indx = -1
         
         for i in range(N-2,-1,-1):
             if nums[i] < nums[i+1]:
-                indx = i
-                break
-        
-        
-        if indx == -1:
-            nums.reverse()
-            return  nums
-        
-        
-        for j in range(N-1,indx,-1):
-            if nums[indx] < nums[j]:
-                nums[indx] , nums[j] = nums[j] , nums[indx]
+                indx  = i
                 break
                 
+        if indx == -1:
+            nums.reverse()
+            return nums
+        
+        for j in range(N-1,indx,-1):
+            if nums[j] > nums[indx]:
+                nums[j] , nums[indx] = nums[indx] , nums[j]
+                break
+        
         # Step 3: reverse the right half:
         nums[indx+1:] = reversed(nums[indx+1:])  
         return nums
