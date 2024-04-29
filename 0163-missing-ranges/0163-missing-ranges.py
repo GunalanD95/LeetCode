82696d6621871dcdf989b70f351617eeaed97f24
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-04-29 06:53:00 +0530

    Time: 41 ms (15.20%), Space: 16.7 MB (13.55%) - LeetHub

--- 0163-missing-ranges/0163-missing-ranges.py
+++ 0163-missing-ranges/0163-missing-ranges.py
@@ -1,32 +1,24 @@
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[List[int]]:
-        if not nums:
-            return [[lower,upper]]
-        if lower == upper:
-            return []
+        n = len(nums)
+        if not nums: return [[lower,upper]]
+        if lower == upper: return []
         
+        missing_ranges = []
         
-        N = len(nums)
-        
-        if N == 1:
-            if lower <= nums[0] <= upper:
-                if nums[0] == lower:
-                    return [[nums[0] + 1, upper]]
-                elif nums[0] == upper:
-                    return [[lower, nums[0] - 1]]
-                else:
-                    return [[lower, nums[0] - 1], [nums[0] + 1, upper]]
-        
-        res = []
         if nums[0] > lower:
-            res.append([lower, nums[0] - 1])        
-        for i in range(1,N):
-            if abs(nums[i-1] - nums[i]) <= 1:
+            missing_ranges.append([lower, nums[0] - 1])
+            
+        
+        for i in range(n-1):
+            if nums[i+1] - nums[i] <= 1:
                 continue
-            cur = [nums[i-1]+1,nums[i]-1]
-            res.append(cur)
+            missing_ranges.append([nums[i]+1,nums[i+1]-1])
+        
         
         if nums[-1] < upper:
-            cur = [nums[N-1]+1,upper]
-            res.append(cur)
-        return res
+            missing_ranges.append([nums[-1]+1,upper])
+            
+            
+        return missing_ranges
+        
