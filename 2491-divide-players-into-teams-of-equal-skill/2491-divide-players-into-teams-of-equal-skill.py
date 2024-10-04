004e49414cbb717afa1bc079bbc6267b50d3700e
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-10-04 07:53:04 +0530

    Time: 597 ms (5.13%), Space: 30 MB (22.65%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,23 +1,36 @@
+from collections import Counter
+
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         N = len(skill)
         
-        skill.sort()
-        
-        prev_sum = None
-        cur_sum  = 0
-        low = 0 
-        high = N-1
-        tot_prod = 0
-        while low < high:
-            cur_sum = skill[low] + skill[high]
-            tot_prod += (skill[low] *  skill[high])
-            if prev_sum and cur_sum != prev_sum:
-                return -1
-            prev_sum = cur_sum 
-            low  += 1
-            high -= 1
         
         
+        total_sum = sum(skill)
         
-        return tot_prod
+        target_sum  = total_sum // (len(skill)//2)
+        
+        skillMap = Counter(skill)
+        
+        print(target_sum)
+        # if target_sum & 1: return -1
+        
+        
+        total_prod = 0
+        for num in skill:
+            need_partner_skill = target_sum - num
+            
+            print(num,need_partner_skill)
+            if need_partner_skill not in skillMap:
+                return - 1
+            
+            
+            skillMap[need_partner_skill] -= 1
+            if skillMap[need_partner_skill] <= 0:
+                del skillMap[need_partner_skill]
+                
+            
+            total_prod += num * need_partner_skill
+            
+
+        return total_prod // 2
