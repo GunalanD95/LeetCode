2d3fb11d69dc496ae30f04dbf1de21ba8427d7bd
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-10-14 09:03:27 +0530

    Time: 284 ms (57.20%), Space: 30 MB (28.39%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.py
+++ 0480-sliding-window-median/0480-sliding-window-median.py
@@ -1,41 +1,72 @@
-import heapq
-from typing import List
-
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        def move(h1, h2):
-            x, i = heapq.heappop(h1)
-            heapq.heappush(h2, (-x, i))
-
-        def get_med(h1, h2, k):
-            return h1[0][0] * 1. if k & 1 else (h1[0][0] - h2[0][0]) / 2.
-
-        small, large = [], []
-        result = []
-
-        for i, x in enumerate(nums[:k]): 
-            heapq.heappush(small, (-x, i))
-        
-        for _ in range(k - k//2):
-            move(small, large)
-
-        result.append(get_med(large, small, k))
-
-        for i, x in enumerate(nums[k:]):
-            if x >= large[0][0]:
-                heapq.heappush(large, (x, i+k))
-                if nums[i] <= large[0][0]:
-                    move(large, small)
+        n = len(nums)
+        lo = [] # max_heap
+        hi = [] # min_heap
+        to_remove = defaultdict(int)
+        def top(tp):
+            if tp == 'lo':
+                return -1 * lo[0]
             else:
-                heapq.heappush(small, (-x, i+k))
-                if nums[i] >= large[0][0]:
-                    move(small, large)
-            
-            while small and small[0][1] <= i:
-                heapq.heappop(small)
-            while large and large[0][1] <= i:
-                heapq.heappop(large)
-            
-            result.append(get_med(large, small, k))
+                return hi[0]
 
-        return result
+        # do setup
+        for i in range(k):
+            heappush(lo, -1 * nums[i])
+            heappush(hi, top('lo'))
+            heappop(lo)
+            if len(hi) > len(lo):
+                heappush(lo, -1 * top('hi'))
+                heappop(hi)
+
+        res = []
+
+        def get_median():
+            if k % 2 == 1:
+                return top('lo')
+            else:
+                return (top('lo') + top('hi')) / 2
+
+        for i in range(k, n+1):
+            res.append(get_median())
+
+            if i == n:
+                return res
+    
+            balance = 0
+            rem_ele = nums[i - k]
+            add_ele = nums[i]
+            i+=1
+
+            ## remove
+            if rem_ele <= top('lo'):
+                balance -= 1
+            else:
+                balance += 1
+            to_remove[rem_ele] += 1
+
+            ## add num
+            if lo and add_ele <= top('lo'):
+                balance += 1
+                heappush(lo, -1 * add_ele)
+            else:
+                balance -= 1
+                heappush(hi, add_ele)
+            
+            # balance
+            if balance < 0:
+                heappush(lo, -1 * top('hi'))
+                heappop(hi)
+                balance += 1
+            elif balance > 0:
+                heappush(hi, top('lo'))
+                heappop(lo)
+                balance -= 1
+            
+            while to_remove[top('lo')] != 0:
+                to_remove[top('lo')]-=1
+                heappop(lo)
+            while hi and to_remove[top('hi')] != 0:
+                to_remove[top('hi')]-=1
+                heappop(hi)
+        return res
