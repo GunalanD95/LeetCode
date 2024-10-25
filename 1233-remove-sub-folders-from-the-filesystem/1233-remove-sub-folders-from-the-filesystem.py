494f5404bca426004886cf7c251ebb2f51aae888
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-10-25 06:36:19 +0530

    Time: 159 ms (68.62%), Space: 42.6 MB (15.17%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,24 +1,56 @@
 from collections import defaultdict
 
+class Node:
+    def __init__(self,is_end=False):
+        self.edges = {}
+        self.is_end = is_end
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         N = len(folder)
         
-        folder.sort(key=lambda x: len(x))
+        root_node = Node()
         
-        folderMap = defaultdict(int)
+        for path in folder:
+            cur_node = root_node
+            folders  = path.split("/")
+            
+            
+            for foldername in folders:
+                if foldername == '':
+                    continue
+                    
+                if foldername not in cur_node.edges:
+                    cur_node.edges[foldername] = Node()
+                    
+                cur_node = cur_node.edges[foldername]
+                
+            cur_node.is_end = True
+            
+        res = []
         
-        
-        for subfolder in folder:
-            cur_char = subfolder.split('/')
-            root_folder = ''
-            flag = True
-            for char in cur_char:
-                if char != '':
-                    root_folder += '/'+char
-                if root_folder in folderMap:
-                    flag = False
+        for path in folder:
+            cur_node = root_node
+            folders  = path.split('/')
+            
+            is_sub_folder = False
+            
+            
+            for i,foldername in enumerate(folders):
+                if foldername == '':
+                    continue
+                    
+                next_node = cur_node.edges[foldername]
+                
+                if next_node.is_end and i != len(folders)-1:
+                    is_sub_folder = True
                     break
-            if root_folder not in folderMap:
-                folderMap[root_folder] += 1       
-        return list(folderMap.keys())
+                
+                cur_node = next_node
+                
+            if not is_sub_folder:
+                res.append(path)
+            
+            
+        
+        return res
