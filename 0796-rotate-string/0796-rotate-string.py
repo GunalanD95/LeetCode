48eb5c90e3a008ee5dafd38aa0ee84bf98a3b3df
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-11-03 06:16:26 +0530

    Time: 0 ms (100.00%), Space: 16.7 MB (9.22%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,34 +1,47 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        N , M = len(s) , len(goal)
-        
-        if N != M:
+        N , K = len(s) , len(goal)
+        if N != K:
             return False
         
+        MOD  = 1000000007
+        base = 27
+        POW  = [1] * (max(N, K) + 1)
+        
+        for i in range(max(N, K)):
+            POW[i+1] = (POW[i] * base)%MOD
+            
+            
+        def generate_hash(w):
+            n = len(w)
+            code = 0
+            for i in range(n):
+                cur_char_acsci = ord(w[i]) - ord('a') + 1
+                value = cur_char_acsci * POW[n-i-1]
+                code += value
+                code %= MOD
+            return code
+        
+        pattern_hash = generate_hash(goal)
         
         
-        for st in range(N):
-            for end in range(M):
-                if s[st] == goal[end]:
-                    i = st
-                    j = end
-                    
-                    count = 0
-                    while count < N:
-                        print(count,s[i],goal[j])
-                        if s[i] == goal[j]:
-                            i += 1
-                            j += 1
-                        else:
-                            break
-                            
-                        count += 1
-                        i %= N
-                        j %= M
-                        
-                        
-                    if count == N:
-                        return True
-                    
-                    
+        s = s + s
+        hash_sum = generate_hash(s[:K])
+        
+        if pattern_hash == hash_sum: return True
+        
+        
+        left , right = 0 , K
+        while right < N+N:
+            hash_sum -= ((ord(s[left]) - ord('a') + 1) * POW[K-1])
+            hash_sum *= (base)  % MOD
+            hash_sum  %= MOD
+            
+            hash_sum += (( ord(s[right]) - ord('a') + 1 ))
+            if pattern_hash == hash_sum: return True
+            left  += 1
+            right += 1
+        
         return False
+        
+                
