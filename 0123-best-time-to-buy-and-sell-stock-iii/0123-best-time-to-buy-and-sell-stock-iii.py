23770e44f6454629016947331673c113b78713b8
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2024-12-01 10:20:39 +0530

    Time: 1574 ms (16.44%), Space: 601.9 MB (5.01%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        N  = len(prices)
+        k  = 2
+        @cache
+        def find_max_profit(indx,trans,holdings):
+            if indx >= N or trans >= k:
+                return 0
+            
+            # two cases
+            # if have holding we can sell
+            # if not we can buy
+            do_nothing  = find_max_profit(indx+1,trans,holdings)
+            do_something = 0
+            if holdings:
+                # we cant buy new one but can we sell or not sell
+                do_something =  prices[indx] + find_max_profit(indx+1,trans+1,0) 
+            else:
+                do_something  = -prices[indx] + find_max_profit(indx+1,trans,1)
+            
+            return max(do_nothing,do_something)
+        
+        
+        return find_max_profit(0,0,0)        
