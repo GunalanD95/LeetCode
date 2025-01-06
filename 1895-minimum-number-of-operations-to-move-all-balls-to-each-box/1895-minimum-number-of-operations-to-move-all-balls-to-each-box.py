b6a8593ba243db379dc6c598f96f97db9ef2b5d5
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2025-01-06 08:36:34 +0530

    Time: 7 ms (89.2%), Space: 18.2 MB (16.05%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,24 +1,22 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        n = len(boxes)
-        answer = [0] * n
+        res=[0]*len(boxes)
 
-        balls_to_left = 0
-        moves_to_left = 0
-        balls_to_right = 0
-        moves_to_right = 0
-
-        # Single pass: calculate moves from both left and right
-        for i in range(n):
-            # Left pass
-            answer[i] += moves_to_left
-            balls_to_left += int(boxes[i])
-            moves_to_left += balls_to_left
-
-            # Right pass
-            j = n - 1 - i
-            answer[j] += moves_to_right
-            balls_to_right += int(boxes[j])
-            moves_to_right += balls_to_right
-
-        return answer
+        # Loop Forwards while calculating right-to-left ball movements
+        ballsBeingMoved=0
+        moves=0
+        for i in range(len(boxes)):
+            moves+=ballsBeingMoved
+            res[i]+=moves
+            if boxes[i]=="1":
+                ballsBeingMoved+=1
+        
+        # Loop Backwards while calculating left-to-right ball movements
+        ballsBeingMoved=0
+        moves=0
+        for i in range(len(boxes)-1, -1, -1):
+            moves+=ballsBeingMoved
+            res[i]+=moves
+            if boxes[i]=="1":
+                ballsBeingMoved+=1
+        return res
