79eb38043485ca1b9ef4d0cf949098d9182c4089
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2025-03-22 07:14:43 +0530

    Time: 55 ms (46.85%), Space: 18.4 MB (53.54%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -3,20 +3,21 @@ class Solution:
     def bfs(self, src):
         q = deque()
         q.append(src)
-        nodes = []
+        self.visited.add(src)
+        size = 0
+        edges_count = 0
         while q:
             for _ in range(len(q)):
                 cur_node = q.popleft()
-                if cur_node in self.visited:
-                    continue
-                self.visited.add(cur_node)
-                nodes.append(cur_node)
+                size += 1
+                edges_count += len(self.graph[cur_node])
                 for child in self.graph[cur_node]:
                     if child not in self.visited:
                         q.append(child)
-
-        print("NODES",nodes)
-        return nodes
+                        self.visited.add(child)
+        edges_count //= 2
+        check = size * (size -1)//2
+        return check == edges_count
 
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
         self.graph = defaultdict(set)
@@ -24,25 +25,9 @@ class Solution:
         for u,v in edges:
             self.graph[u].add(v)
             self.graph[v].add(u)
-
-        print("graph",self.graph)
-        components = []
+        count = 0
         for node in range(n):
             if node not in self.visited:
-                components.append(self.bfs(node))
-
-        count = 0
-        for nodes in components:
-            connected = True
-            for i in range(len(nodes)):
-                for j in range(i+1,len(nodes)):
-                    u = nodes[i]
-                    v = nodes[j]
-                    print(u,self.graph[v],v)
-                    if u not in self.graph[v]:
-                        connected = False
-                        break
-
-            if connected:
-                count += 1
+                if self.bfs(node):
+                    count += 1
         return count    
