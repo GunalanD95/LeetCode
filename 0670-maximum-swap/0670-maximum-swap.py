a249bc85a85e4951c8a8dd7f79055b79ce768f44
Gunalan D <54971983+GunalanD95@users.noreply.github.com>
2025-03-22 10:59:50 +0530

    Time: 0 ms (100%), Space: 17.9 MB (14.03%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.py
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -1,20 +1,14 @@
 class Solution:
     def maximumSwap(self, num: int) -> int:
         nums = list(str(num))
-        MAX_NUM = num
-        print("Nums",nums)
-        N = len(nums)
-        if N == 1:
-            return num
-        max_number_idx = N-1
-        for idx in range(N-2,-1,-1):
-            if nums[idx] > nums[max_number_idx]:
-                max_number_idx = idx
-            else:
-                nums[idx] , nums[max_number_idx] = nums[max_number_idx] , nums[idx]
-                print("nums",nums)
-                CUR_NUM = "".join(nums)
-                CUR_NUM = int(CUR_NUM)
-                MAX_NUM = max(MAX_NUM,int(CUR_NUM))
-                nums[max_number_idx] , nums[idx] = nums[idx] , nums[max_number_idx]
-        return MAX_NUM
+        last_occurence = {int(num):idx for idx,num in enumerate(nums)}
+
+        for idx in range(len(nums)):
+            cur_num = int(nums[idx])
+            for number in range(9,cur_num,-1):
+                if number in last_occurence and last_occurence[number] > idx:
+                    nums[idx],nums[last_occurence[number]] = nums[last_occurence[number]],nums[idx]
+                    return int("".join(nums))
+
+        
+        return num
